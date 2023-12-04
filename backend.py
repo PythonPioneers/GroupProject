60120e1dfee26a85f6bf701dd52f0a4f5ca1fbd2
ajayramsunder1234 <ajayr4@illinois.edu>
2023-12-04 17:58:46 -0600

    style changes and comments on code

--- backend.py
+++ backend.py
@@ -1,30 +1,33 @@
+# import flask and cors to make sure that the development server will launch when the file is run and can work on Google Chrome
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+# import nltk and joblib to load the saved version of the model that will predict on the comment that is fed in
 import nltk 
 import joblib
 
-model = joblib.load('/Users/ethanshen/Documents/UIUC/Fa23/CS410/GroupProject/unigram_xgboost_oversample.joblib')
-vectorizer = joblib.load('/Users/ethanshen/Documents/UIUC/Fa23/CS410/GroupProject/vectorizer.joblib')
+# load the model and vectorizer which will predict and break down the words, also make sure stopwords are known 
+model = joblib.load('unigram_xgboost_oversample.joblib')
+vectorizer = joblib.load('vectorizer.joblib')
 nltk.download('stopwords')
 stopwords = nltk.corpus.stopwords.words("english")
 
+# these are headers for the flask methods to make sure the server runs and browser allows 
+# for the connection between the frontend and the backend
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
-# IMPORT JOBLIB MODEL HERE
-
+# headers for functions to act as endpoints in the server so that way if the endpoint is called, the function is executed
 @app.route('/model_change', methods=['POST'])
 @cross_origin(supports_credentials=True)
-
-
-def your_endpoint():
+# function that takes in the comment passed from the review and predicts whether it is positive, negative, or neutral
+def model_change():
     try:
+        # data is a json that holds the data passed from the frontend
         data = request.get_json()
-        id = data["ID"]
+        # elements of the data json
         comment = data["comments"]
-        rating = data["Rating"]
         title = data["title"]
-        # the data is in a json for one review, and the variables are shown here implement model stuff here
+        # the prediction and assignment of sentiment
         input_vector = vectorizer.transform([comment])
         prediction = model.predict(input_vector)
         assert prediction in [0,1,2]
@@ -34,11 +37,11 @@ def your_endpoint():
             return 'Neutral'
         elif prediction == 2:
             return 'Positive'
-        
-        # print(id, comment, rating, title)
-        #return prediction
+        # returns of the frontend
+        return prediction
+    # this is there to make sure if there is a problem with the server it will throw an error
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
+# main function to make sure that the server runs as a server
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="bg-gray-900 text-white p-4">
       <div className="container mx-auto">
         <h1 className="text-3xl font-bold">Amazon Sentiment Analysis</h1>
       </div>
